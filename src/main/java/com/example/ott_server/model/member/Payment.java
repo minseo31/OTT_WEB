8f40c9f0b125a6b0d7ad001c0ea374111e8a5939
minseo <manseo0731@gmail.com>
2024-07-23 10:37:04 +0900

    Update Payment.java

--- src/main/java/com/example/ott_server/model/member/Payment.java
+++ src/main/java/com/example/ott_server/model/member/Payment.java
@@ -23,7 +23,7 @@ public class Payment {
     private String expiry_date;
 
     @Column(name = "payment_date")
-    private String payment_date;
+    private LocalDate payment_date;
 
     @Column(name = "card_name")
     private String card_name;
