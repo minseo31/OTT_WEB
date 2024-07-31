50831ab6e8673a078dc46c7816b84e1c3dbe2c47
minseo <manseo0731@gmail.com>
2024-07-31 10:04:37 +0900

    \

--- src/components/icon/Arrowicon.tsx
+++ src/components/icon/Arrowicon.tsx
@@ -5,7 +5,7 @@ import { ArrowiconPropsType } from '../../type/props/atom';
 // 화살표 아이콘 - direction은 rotate방향 값입니다. (rotate-180 or rotate-0)
 const Arrowicon = ({ direction, onClick }: ArrowiconPropsType) => {
   return (
-    <div className={`${iconStyle} ${direction} z-[98] `} onClick={onClick}>
+    <div className={`${iconStyle} ${direction} z-[50] `} onClick={onClick}>
       <MdPlayArrow />
     </div>
   );
