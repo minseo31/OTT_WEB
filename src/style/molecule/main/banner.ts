056e4687864ec0d5dc80de6c72f6fdd7555f6fbc
minseo <manseo0731@gmail.com>
2024-08-01 10:12:55 +0900

    Update banner.ts

--- src/style/molecule/main/banner.ts
+++ src/style/molecule/main/banner.ts
@@ -1,6 +1,6 @@
 // 메인 배너 컨테이너
 export const MainBannerContainerStyle =
-  'w-full h-[300px] px-12 flex justify-center gap-4 items-center relative mt-[20px] ';
+  'w-full h-[350px] px-12 flex justify-center gap-4 items-center relative mt-[20px] ';
 
 export const mainBannerBoxStyle = 'w-full h-full overflow-hidden';
 
