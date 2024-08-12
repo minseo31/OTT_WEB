c3353e7e4817b0fa284b0a645486a6c0c845cbce
minseo <manseo0731@gmail.com>
2024-08-12 17:47:50 +0900

    Update WebConfig.java

--- src/main/java/com/example/ott_server/config/WebConfig.java
+++ src/main/java/com/example/ott_server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://delightful-ground-09b241710.5.azurestaticapps.net")
+                .allowedOrigins("http://localhost:3000", "https://victorious-dune-0cd4ffa10.5.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
